330e789c09b4413ec06653cc71c4598d378bebbf
OhGarin <melissanessigarcia@gmail.com>
2024-12-08 21:46:40 -0400

    formulario catalogo floristeria primera version

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,9 +1,9 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-
+import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, NgbDropdownModule],
   standalone: true,
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
