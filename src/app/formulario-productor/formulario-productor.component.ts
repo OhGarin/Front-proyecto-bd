dc6efbf0954080cc2f8270b0e73dc8ab55fae219
OhGarin <melissanessigarcia@gmail.com>
2024-12-10 19:29:58 -0400

    modif productor

--- src/app/formulario-productor/formulario-productor.component.ts
+++ src/app/formulario-productor/formulario-productor.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { NgbDropdownModule } from '@ng-bootstrap/ng-bootstrap';
 import { BackendModule } from '../backend/backend.module';
+import { Color, Productor } from '../backend/types';
 import { BackendService } from '../backend/backend.service';
 import { FormsModule } from '@angular/forms';
 
@@ -15,4 +16,15 @@ export class FormularioProductorComponent implements OnInit {
   constructor(private backendService: BackendService) {}
 
   ngOnInit(): void {}
+
+  setProductor(id: number, nombre: string) {
+    this.idProductorSeleccionado = id;
+    this.nombreProductorSeleccionado = nombre;
+  }
+
+  setCodigoColor(codigo: string, nombre: string) {
+    this.codigoColorSeleccionado = codigo;
+    this.nombreColorSeleccionado = nombre;
+  }
+
 }
